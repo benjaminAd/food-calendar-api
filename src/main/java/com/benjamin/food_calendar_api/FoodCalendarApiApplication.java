dcba1c0a71922428f946e0ea0a0f69721c4f3811
benjamin <adolphe906@gmail.com>
2025-11-23 15:57:32 +0100

    feat: :sparkles: initialize Spring Boot project with Maven and Dev Container setup.

---
+++ src/main/java/com/benjamin/food_calendar_api/FoodCalendarApiApplication.java
@@ -0,0 +1,13 @@
+package com.benjamin.food_calendar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCalendarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCalendarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benjamin/food_calendar_api/FoodCalendarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjamin.food_calendar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCalendarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
